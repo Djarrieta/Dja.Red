3c25e7cad80810d38dead3b5f06fd60110790dc4
Dario Arrieta <arrieta.dario@hotmail.com>
2020-05-14 11:05:26 -0500

    headers y action button ok, falta footer y main2

--- js/js.js
+++ js/js.js
@@ -1,3 +1,13 @@
-(document).ready(function(){
-    $('.collapsible').collapsible();
+//NavBar
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
   });
+  //button floating
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems,{
+        toolbarEnabled:true
+    });
+  });
+
