29a373109b6aa6d037680bfe7314c1d17c1469b7
Dario Arrieta <arrieta.dario@hotmail.com>
2020-05-16 11:23:58 -0500

    errores corregidos

--- js/js.js
+++ js/js.js
@@ -11,21 +11,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-
-function GetData(){
-    var n=document.getElementById("numberDj").value;
-    var e=document.getElementById('emailDj').value;
-    if(!n&&!e){
-        Email.send
-        ({
-            SecureToken : "6AAF8E03C62A08D220DA6A923714202F6383",
-            To : 'djarrieta@dja.red',
-            From : "djarrieta@dja.red",
-            Subject : "This is the subject",
-            Body : "And this is the body",
-        }).then(
-          message => alert(message)
-        );
-    }
-    
-}
