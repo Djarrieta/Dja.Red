8a79e8d4b1573489466d9dd4cd8c591058fa9c8e
Dario Arrieta <arrieta.dario@hotmail.com>
2020-05-19 16:14:41 -0500

    me di cuenta que master si esta funcionando bien. Voy a dejar esta hasta aca por ahora

--- js/js.js
+++ js/js.js
@@ -1,113 +1,37 @@
-//LATERAL
-
-var menu=document.getElementById('header--menuSandwitch')
-var menuDesplegable=document.getElementById('menuDesplegable')
-var left= window.getComputedStyle(menuDesplegable,null).getPropertyValue("left");
-
-menu.addEventListener('click',HideShowMenu)
-
-function HideShowMenu(){
-
-  if(window.getComputedStyle(menuDesplegable,null).getPropertyValue("left")!='0px'){
-    menuDesplegable.classList.add('menuDesplegableShow')
-  }else{
-    menuDesplegable.classList.remove('menuDesplegableShow')
-   }
-}
-
-//MENU FLOTANTE
-var menuFlotante=document.getElementById('menuFlotante')
-var botonFlotante=document.getElementById('floatingButton')
-var leftFlotante= window.getComputedStyle(menuDesplegable,null).getPropertyValue("left");
-
-botonFlotante.addEventListener('click',ShowMenuFlotante);
-menuFlotante.addEventListener('click',HideBotonFlotante)
-
-function ShowMenuFlotante(){
-  menuFlotante.classList.add('menuFlotanteShow')
-  botonFlotante.classList.add('floatingButtonHide')
-}
-function HideBotonFlotante(){
-  menuFlotante.classList.remove('menuFlotanteShow')
-  botonFlotante.classList.remove('floatingButtonHide')
-}
-
-//CARDS SUBMENU
-var obj=document.getElementById('card1')
-obj.addEventListener('click',ShowHideObj)
-
-function ShowHideObj(o){
-  console.log(o)
-  o.hidden=true
-}
-
-
-/* var obj=document.getElementById('card1')
-obj.addEventListener('click',ShowHideObj)
-
-function ShowHideObj(){
-  console.log(obj.nextSibling.nextElementSibling.id)
-} */
-
-
-
-
-
-/* 
-function HideMenuFlotante(){
-  if(window.getComputedStyle(menuFlotante,null).getPropertyValue("left")!='0px'){
-    menuFlotante.classList.add('menuFlotanteShow')
-    menuDesplegable.classList.remove('menuDesplegableShow')
-    botonFlotante.classList.add('floatingButtonHide')
+function elemento(e){
+  var obj=e.srcElement
+  if(obj){ 
+    switch(obj.id){
+      case 'MenuSandwitch':
+        var menuDesplegable=document.getElementById('menuDesplegable')
+        if(!menuDesplegable.classList.contains('menuDesplegableShow')){
+          menuDesplegable.classList.add('menuDesplegableShow')
+        }else{
+          menuDesplegable.classList.remove('menuDesplegableShow')
+        }
+        break;
+      case 'BotonFlotante':
+        var menuFlotante=document.getElementById('menuFlotante')
+        var floatingButton=document.getElementById('floatingButton')
+        menuFlotante.classList.add('menuFlotanteShow')
+        floatingButton.classList.add('floatingButtonHide')
+        break;
+      case 'menuFlotante':
+        var menuFlotante=document.getElementById('menuFlotante')
+        var floatingButton=document.getElementById('floatingButton')
+        menuFlotante.classList.remove('menuFlotanteShow')
+        floatingButton.classList.remove('floatingButtonHide')
+        break;
+      case 'card--principal__img':
+      case 'card--principal__text':
+        var cardPrincipal1=document.getElementById('card--principal1')
+        cardPrincipal1.classList.add('hideCard--principal')
+        break;
+      case 'card--secundario__p':
+        var cardPrincipal1=document.getElementById('card--principal1')
+        cardPrincipal1.classList.remove('hideCard--principal')
+        break;
+    }
   }
 }
 
-function ShowBotonFlotante(){
-  botonFlotante.classList.remove('floatingButtonHide')
-  menuFlotante.classList.remove('menuFlotanteShow')
-  menuDesplegable.classList.remove('menuDesplegableShow')
-}
-window.onscroll=function(e){
-  menuFlotante.classList.remove('menuFlotanteShow')
-  menuDesplegable.classList.remove('menuDesplegableShow')
-} */
-
-
-
-
-// You could now get your value like
-
-
-
-
-/* //NavBar
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
-  //button floating
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems,{
-        toolbarEnabled:true
-    });
-  });
-
-
-function GetData(){
-    var n=document.getElementById("numberDj").value;
-    var e=document.getElementById('emailDj').value;
-    if(!n&&!e){
-        Email.send
-        ({
-            SecureToken : "6AAF8E03C62A08D220DA6A923714202F6383",
-            To : 'djarrieta@dja.red',
-            From : "djarrieta@dja.red",
-            Subject : "This is the subject",
-            Body : "And this is the body",
-        }).then(
-          message => alert(message)
-        );
-    }
-    
-} */
