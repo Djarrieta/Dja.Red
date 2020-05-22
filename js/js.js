dc12e2e530f5bda8b2f9ab4afff0b7111f6de6bd
Dario Arrieta <arrieta.dario@hotmail.com>
2020-05-22 15:23:15 -0500

    Se organizó la página para móviles. Falta agregar configuraciones para pantallas grandes y web.

--- js/js.js
+++ js/js.js
@@ -22,15 +22,34 @@ function elemento(e){
         menuFlotante.classList.remove('menuFlotanteShow')
         floatingButton.classList.remove('floatingButtonHide')
         break;
-      case 'card--principal__img':
-      case 'card--principal__text':
+      case 'card--principal1__img':
+      case 'card--principal1__text':
         var cardPrincipal1=document.getElementById('card--principal1')
         cardPrincipal1.classList.add('hideCard--principal')
         break;
-      case 'card--secundario__p':
+      case 'card--secundario1__p':
         var cardPrincipal1=document.getElementById('card--principal1')
         cardPrincipal1.classList.remove('hideCard--principal')
         break;
+      case 'card--principal2__img':
+      case 'card--principal2__text':
+        var cardPrincipal2=document.getElementById('card--principal2')
+        cardPrincipal2.classList.add('hideCard--principal')
+        break;
+      case 'card--secundario2__p':
+        var cardPrincipal2=document.getElementById('card--principal2')
+        cardPrincipal2.classList.remove('hideCard--principal')
+        break;
+      case 'card--principal3__img':
+      case 'card--principal3__text':
+        var cardPrincipal3=document.getElementById('card--principal3')
+        cardPrincipal3.classList.add('hideCard--principal')
+        break;
+      case 'card--secundario3__p':
+        var cardPrincipal3=document.getElementById('card--principal3')
+        cardPrincipal3.classList.remove('hideCard--principal')
+        break;
+          
     }
   }
 }
